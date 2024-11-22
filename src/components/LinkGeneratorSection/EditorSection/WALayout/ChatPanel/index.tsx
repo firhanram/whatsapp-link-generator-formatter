41039f2716628182cd4b48636211e454d8766525
Firhan Ramadhan <rfirhan99@gmail.com>
2024-11-22 19:57:11 +0700

    feat: adjust styling

--- src/components/LinkGeneratorSection/EditorSection/WALayout/ChatPanel/index.tsx
+++ src/components/LinkGeneratorSection/EditorSection/WALayout/ChatPanel/index.tsx
@@ -1,16 +1,13 @@
 import BubleChatEditor from "./BubleChatEditor";
 import FloatingToolbar from "./FloatingToolbar";
-import TimeBadge from "./TimeBadge";
 import { type Editor } from "@tiptap/react";
 
 const ChatPanel = ({ editor }: { editor: Editor }) => {
   return (
     <div className="bg-conversation-panel flex-grow flex-shrink order-2 relative">
       <div className="absolute inset-0 z-30 h-full w-full pb-2 overflow-x-hidden overflow-y-scroll">
-        <div className="min-h-3 w-full" />
+        <div className="min-h-10 w-full" />
         <div className="px-[63px]">
-          <TimeBadge />
-
           <div className="flex justify-center sticky top-4 z-10">
             <FloatingToolbar editor={editor} />
           </div>
--- src/components/LinkGeneratorSection/EditorSection/WALayout/HeaderPanel/index.tsx
+++ src/components/LinkGeneratorSection/EditorSection/WALayout/HeaderPanel/index.tsx
@@ -1,8 +1,12 @@
+import { StoreIcon } from "lucide-react";
+
 const HeaderPanel = () => {
   return (
-    <header className="flex py-2 px-4 items-center gap-4 bg-panel-header relative z-30">
-      <div className="size-10 rounded-full bg-slate-300" />
-      <div className="text-base text-primary">John Doe</div>
+    <header className="flex py-2 px-4 items-center gap-4 bg-primary relative z-30">
+      <div className="size-10 rounded-full bg-secondary flex items-center justify-center shrink-0">
+        <StoreIcon className="text-primary" />
+      </div>
+      <div className="text-base text-secondary">Your Business</div>
     </header>
   );
 };
