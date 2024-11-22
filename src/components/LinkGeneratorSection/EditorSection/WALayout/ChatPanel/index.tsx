03e1147c35a696a1b79caf6e38829455a6f7c644
Firhan Ramadhan <rfirhan99@gmail.com>
2024-11-22 13:07:56 +0700

    feat: add sticky

--- src/components/LinkGeneratorSection/EditorSection/WALayout/ChatPanel/index.tsx
+++ src/components/LinkGeneratorSection/EditorSection/WALayout/ChatPanel/index.tsx
@@ -5,13 +5,13 @@ import { type Editor } from "@tiptap/react";
 
 const ChatPanel = ({ editor }: { editor: Editor }) => {
   return (
-    <div className="pb-2 bg-conversation-panel flex-grow flex-shrink order-2 relative">
-      <div className="absolute inset-0 z-30 h-full w-full overflow-x-hidden overflow-y-scroll">
+    <div className="bg-conversation-panel flex-grow flex-shrink order-2 relative">
+      <div className="absolute inset-0 z-30 h-full w-full pb-2 overflow-x-hidden overflow-y-scroll">
         <div className="min-h-3 w-full" />
         <div className="px-[63px]">
           <TimeBadge />
 
-          <div className="flex justify-center">
+          <div className="flex justify-center sticky top-4 z-10">
             <FloatingToolbar editor={editor} />
           </div>
 
