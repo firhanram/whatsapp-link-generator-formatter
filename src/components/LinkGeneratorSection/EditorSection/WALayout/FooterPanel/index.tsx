649157dce75e2f05a973446a987e0c9ed9515c61
Firhan Ramadhan <rfirhan99@gmail.com>
2024-11-22 19:47:08 +0700

    feat: remove modal and move input

--- src/components/LinkGeneratorSection/EditorSection/WALayout/FooterPanel/index.tsx
+++ src/components/LinkGeneratorSection/EditorSection/WALayout/FooterPanel/index.tsx
@@ -1,11 +1,4 @@
 import { ArrowDownToLineIcon } from "lucide-react";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/Dialog";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 import { useState } from "react";
@@ -55,69 +48,49 @@ const FooterPanel = ({ editor }: { editor: Editor }) => {
   };
 
   return (
-    <footer className="order-3 px-4 h-[64px] py-[9px] bg-[#f0f2f5] relative z-30 flex justify-between gap-4">
-      <div className="bg-white rounded-lg flex-1" />
-      <div className="flex items-center justify-center">
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button>
-              <ArrowDownToLineIcon />
-              Generate Link
-            </Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Generate WhatsApp Link</DialogTitle>
-            </DialogHeader>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                const formData = new FormData(e.target as HTMLFormElement);
-                const phoneNumber = formData.get("phoneNumber") as string;
+    <footer className="order-3 px-4 min-h-[64px] py-[9px] bg-white relative z-30">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          const phoneNumber = formData.get("phoneNumber") as string;
 
-                const encodedText = encodeURIComponent(
-                  toWhatsappText(editor.getJSON())
-                );
+          const encodedText = encodeURIComponent(
+            toWhatsappText(editor.getJSON())
+          );
 
-                console.log(
-                  `${WHATSAPP_LINK}${phoneNumber}?text=${encodedText}`
-                );
-              }}
-            >
-              <div className="space-y-1">
-                <Input
-                  placeholder="Ex: 6281234567890 or 081234567890"
-                  required
-                  name="phoneNumber"
-                  onChange={(e) => {
-                    setPhoneNumber(e.target.value);
-                  }}
-                  value={phoneNumber}
-                  onInput={(e) => {
-                    const target = e.target as HTMLInputElement;
-                    const { value } = target;
+          console.log(`${WHATSAPP_LINK}${phoneNumber}?text=${encodedText}`);
+        }}
+        className="flex justify-between gap-4"
+      >
+        <div className="flex-1 space-y-1">
+          <Input
+            placeholder="Input your phone number here"
+            required
+            name="phoneNumber"
+            onChange={(e) => {
+              setPhoneNumber(e.target.value);
+            }}
+            value={phoneNumber}
+            onInput={(e) => {
+              const target = e.target as HTMLInputElement;
+              const { value } = target;
 
-                    if (
-                      Number(value) !== 0 &&
-                      !Number(value) &&
-                      value.length > 0
-                    ) {
-                      target.value = value.slice(0, value.length - 1);
-                    }
-                  }}
-                />
-                <p className="text-muted-foreground font-medium text-xs">
-                  Will redirect to: {`${WHATSAPP_LINK}${phoneNumber}`}
-                </p>
-              </div>
-
-              <div className="flex justify-end mt-3">
-                <Button>Generate</Button>
-              </div>
-            </form>
-          </DialogContent>
-        </Dialog>
-      </div>
+              if (Number(value) !== 0 && !Number(value) && value.length > 0) {
+                target.value = value.slice(0, value.length - 1);
+              }
+            }}
+          />
+          <p className="text-muted-foreground text-xs">
+            Enter your phone number to generate a WhatsApp link. Example:
+            6281234567890 or 081234567890
+          </p>
+        </div>
+        <Button type="submit">
+          <ArrowDownToLineIcon />
+          Generate Link
+        </Button>
+      </form>
     </footer>
   );
 };
