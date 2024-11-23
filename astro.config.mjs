c94b828e9ecbffc8465f375998040f503ac824b8
Firhan Ramadhan <rfirhan99@gmail.com>
2024-11-23 19:58:36 +0700

    feat: add seo purpose

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -12,5 +14,6 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     react(),
+    sitemap(),
   ],
 });
--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,3 +1,4 @@
-export const SEO_TITLE = "Free WhatsApp Link Generator – Format & Share Easily";
+export const SEO_TITLE =
+  "Easywalink | Free WhatsApp Link Generator, Format & Share Easily";
 export const SEO_DESC =
   "Generate WhatsApp links with formatted text, including bold, italic, and more. Easily preview and share styled messages in seconds. Create your customized message link today!";
