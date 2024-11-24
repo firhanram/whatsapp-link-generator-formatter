9cc49ead5caaeb8693066ead203044c185459115
Firhan Ramadhan <rfirhan99@gmail.com>
2024-11-24 07:17:19 +0700

    fix: change favicon href and add site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.easywalink.com",
   integrations: [
     tailwind({
       applyBaseStyles: false,
